93154160390e70c3747c9202bedbfca2e88790e1
yahya <yahya.ensas@hotmail.com>
2021-04-14 12:30:59 +0000

    generate vue from path

--- src/main/java/web/ControlleurServlet.java
+++ src/main/java/web/ControlleurServlet.java
@@ -9,17 +9,23 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name = "cs",urlPatterns = {"/controleur","*.php"})
+@WebServlet(name = "cs",urlPatterns = {"/controleur","*.do"})
 public class ControlleurServlet extends HttpServlet {
 
 	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(req, resp);
+	}
+	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-		resp.setContentType("text/html");
-	  //	PrintWriter out = resp.getWriter();
-		String name = req.getParameter("name");
-		req.setAttribute("model", name);
-		//req.getRequestDispatcher("vue.jsp").forward(req, resp);
-		resp.sendRedirect("vue.jsp?name="+name);
+		
+		String path = req.getServletPath();
+		System.out.println(path);
+		String viewName = path.substring(1,path.indexOf(".do"));
+		System.out.println(viewName);
+		String pathView = "views/"+viewName+".jsp";
+		System.out.println(pathView);
+		req.getRequestDispatcher(pathView).forward(req, resp);;
 	}
 }
